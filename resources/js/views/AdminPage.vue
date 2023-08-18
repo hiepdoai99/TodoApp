001aed51608d71473ce3f2d88f00753aab8b69a4
hiepdoai99 <nguyenxuanhiepk49@gmail.com>
2023-08-18 11:48:03 +0700

    save

--- resources/js/views/AdminPage.vue
+++ resources/js/views/AdminPage.vue
@@ -4,6 +4,7 @@ import TabWrapper from '../components/TabWrapper.vue';
 import StatisticComponent from '../components/StatisticComponent.vue';
 import UserTableList from '../components/UserTablelist.vue';
 import TeamTableList from '../components/TeamTableList.vue'
+import Roles from '../components/Permisstions.vue'
 </script>
 
 <template>
@@ -20,6 +21,9 @@ import TeamTableList from '../components/TeamTableList.vue'
 			<TabSection title="Teams table">
 				<TeamTableList/>
 			</TabSection>
+            <TabSection title="Roles table">
+				<Roles/>
+			</TabSection>
 		</TabWrapper>
  </section>
 </template>
@@ -31,7 +35,7 @@ import TeamTableList from '../components/TeamTableList.vue'
 			border-radius: 10px;
 			background-color: #1D5D9B;
     }
-		
+
 		.tab-section{
 			width: 100%;
 		}
