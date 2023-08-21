9bfa1361b734651400ab93e4607f7b470b23e486
hiepdoai99 <nguyenxuanhiepk49@gmail.com>
2023-08-21 17:59:55 +0700

    role

--- resources/js/views/AddTodo.vue
+++ resources/js/views/AddTodo.vue
@@ -37,7 +37,7 @@ const getUserLoginData = () => {
 
 }
 const getUser = () => {
-    $axios.get('/sameTeam').then((data) => {
+    $axios.get('/user').then((data) => {
         users.value = data.data.data
 				console.log('all user data', users.value);
     })
