a3c41b4da326792c81c80122df328b6855c2889d
hiepdoai99 <104878890+hiepdoai99@users.noreply.github.com>
2023-09-18 17:00:40 +0700

    Update request.js

--- resources/js/utils/request.js
+++ resources/js/utils/request.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const $axios = axios.create({
-    baseURL: "https://task.pics/api/v1",
-    // baseURL: 'http://localhost:8000/api/v1',
+    baseURL: "https://api.task.pics/api/v1",
     timeout: 20000,
 });
 
